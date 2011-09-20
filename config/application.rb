5d842d9a1e6069484efe81d7913d0ee050badc81
Steve Crozier <steve@crozier.com>
2011-09-20 00:10:19 -0500

    Change domain to heroku.

--- config/application.rb
+++ config/application.rb
@@ -39,7 +39,7 @@ module Clearing
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
 
-    config.action_mailer.default_url_options = { :host => "concertsintheclearing.org" }
+    config.action_mailer.default_url_options = { :host => "clearing.heroku.com" }
 
   end
 end
